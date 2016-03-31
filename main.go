625a56299329e3dc85f15e415538bccd74542424
Meng Zhuo <mengzhuo1203@gmail.com>
2016-03-31 16:06:51 +0800

    new server wip

--- main.go
+++ main.go
@@ -2,35 +2,11 @@
 package main
 
 import (
-	"chat"
 	"flag"
-	"log"
-	"os"
-	"runtime/pprof"
-	"time"
 )
-
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
 
 	flag.Parse()
 
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		pprof.StartCPUProfile(f)
-
-		go func() {
-			time.Sleep(2 * time.Minute)
-			pprof.StopCPUProfile()
-			log.Printf("Stoped profile")
-			os.Exit(0)
-		}()
-	}
-
-	server := chat.NewChatServer(":12345")
-	server.ListenAndServe()
 }
